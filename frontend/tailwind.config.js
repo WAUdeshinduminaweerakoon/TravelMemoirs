445332d52a1d363ac98980ded6b6722e9a4be03b
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2025-08-15 23:21:34 +0530

    add the tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+        display :["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors:{
+        primary: "#79dffc",
+        secondary: "#363eff",
+      },
+    },
+  },
+  plugins: [],
+}
